1f4568add3a47c0264d88213ce98d4294b1cd4dc
Nghethuathoc <motvitinhtu07@gmail.com>
2025-05-08 23:55:22 +0700

    Add files via upload

---
+++ baovebanquyen.js
@@ -0,0 +1,30 @@
+
+
+document.querySelector('video').setAttribute('oncontextmenu', "return false;")
+
+    $(document).ready(function() {
+        $("video").bind("contextmenu",function(){
+            return false;
+            });
+     } );
+
+
+
+
+
+
+    //  ScrollReveal({ 
+    //     reset: true,
+    //     distance: '60px',
+    //     duration: 2500,
+    //     delay: 400,
+    // });
+
+    // ScrollReveal().reveal('.tieudenhomnguyenlieu', { delay: 500, origin: 'left' });
+    // ScrollReveal().reveal('.effect2', { delay: 200, origin: 'left' });
+    // ScrollReveal().reveal('.effect3', { delay: 200, origin: 'left' });
+    // ScrollReveal().reveal('.effect4', { delay: 800, origin: 'left' });
+
+
+
+    
